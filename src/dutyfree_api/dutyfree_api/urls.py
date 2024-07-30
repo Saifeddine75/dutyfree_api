90233ac78d63217908baebced2d365d6ae68de7e
Saifeddine <saifeddine.bejaoui@gmail.com>
2024-07-30 13:49:56 +0200

    Fix project urls

--- src/dutyfree_api/dutyfree_api/urls.py
+++ src/dutyfree_api/dutyfree_api/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from purchase.views import CustomerViewSet
-
-router = DefaultRouter()
-router.register(r'customers', CustomerViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('v1', include(router.urls))
+    path('', include('purchase.urls')),
 ]
