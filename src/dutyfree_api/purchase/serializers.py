a19334054f0632e38cd01d012bc1bad59f17fd11
Saifeddine <saifeddine.bejaoui@gmail.com>
2024-07-30 13:53:01 +0200

    Add salutation field and exclude customer fk field

--- src/dutyfree_api/purchase/serializers.py
+++ src/dutyfree_api/purchase/serializers.py
@@ -4,19 +4,31 @@ from .models import Customers, Purchases
 class PurchaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Purchases
-        exclude = ['customer']
+        fields = ['product_id', 'price', 'currency', 'quantity', 'purchased_at']
         
 
 class CustomerSerializer(serializers.ModelSerializer):
+    purchased_at = serializers.DateField(format='%Y-%m-%d', input_formats=['%Y-%m-%d'])
+    salutation = serializers.CharField(source='title')
+    purchases = PurchaseSerializer(many=True)
     
     class Meta:
         model = Customers
-        exclude = ['id']
+        fields = ['salutation', 'last_name', 'first_name', 'email', 'purchases', 'purchased_at']
         
     def create(self, validated_data):
-        purchase_data = validated_data.pop('purchases')
-        customer = Customers.objects.create(**validated_data)
-        for purchase in purchase_data:
-            purchase['customer'] = customer
-            Purchases.objects.create(**purchase)
-        return customer
+        print("validated_data", validated_data)
+        salutation = validated_data.get('salutation')
+        if salutation == 'Mrs':
+            validated_data['title'] = 1
+        elif salutation == 'Mr':
+            validated_data['title'] = 2
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        salutation = validated_data.get('salutation', instance.salutation)
+        if salutation == 'Mrs':
+            validated_data['title'] = 1
+        elif salutation == 'Mr':
+            validated_data['title'] = 2
+        return super().update(instance, validated_data)
