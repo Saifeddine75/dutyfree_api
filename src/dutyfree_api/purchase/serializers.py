0471d1a936ec14163dd44ead7434548c9452c4f9
Saifeddine <saifeddine.bejaoui@gmail.com>
2024-07-30 14:54:20 +0200

    Update serializers to create customer before to attach purchases

--- src/dutyfree_api/purchase/serializers.py
+++ src/dutyfree_api/purchase/serializers.py
@@ -8,13 +8,14 @@ class PurchaseSerializer(serializers.ModelSerializer):
         
 
 class CustomerSerializer(serializers.ModelSerializer):
-    purchased_at = serializers.DateField(format='%Y-%m-%d', input_formats=['%Y-%m-%d'])
+    # purchased_at = serializers.DateField(format='%Y-%m-%d', input_formats=['%Y-%m-%d'])
     salutation = serializers.CharField(source='title')
     purchases = PurchaseSerializer(many=True)
     
     class Meta:
         model = Customers
-        fields = ['salutation', 'last_name', 'first_name', 'email', 'purchases', 'purchased_at']
+        fields = ['salutation', 'last_name', 'first_name', 'email', 'purchases']
+        
         
     def create(self, validated_data):
         print("validated_data", validated_data)
@@ -23,8 +24,14 @@ class CustomerSerializer(serializers.ModelSerializer):
             validated_data['title'] = 1
         elif salutation == 'Mr':
             validated_data['title'] = 2
-        return super().create(validated_data)
-    
+        
+        purchases_data = validated_data.pop('purchases')
+        customer = Customers.objects.create(**validated_data)
+        
+        for purchase_data in purchases_data:
+            Purchases.objects.create(customer=customer, **purchase_data)
+        return customer
+
     def update(self, instance, validated_data):
         salutation = validated_data.get('salutation', instance.salutation)
         if salutation == 'Mrs':
