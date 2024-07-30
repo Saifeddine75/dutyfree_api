c74359e31c8aa112c47d5069123b87a375c999f6
Saifeddine <saifeddine.bejaoui@gmail.com>
2024-07-30 09:09:20 +0200

    Update title and quantity attribute

--- src/dutyfree_api/purchase/models.py
+++ src/dutyfree_api/purchase/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Customers(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField() # male / female
+    title = models.IntegerField() # male / female
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique = True)
@@ -16,9 +16,9 @@ class Customers(models.Model):
 class Purchases(models.Model):
     customer =  models.ForeignKey('Customers', related_name='purchases', on_delete=models.CASCADE)
     product_id = models.CharField(max_length=20)
-    price = models.DecimalField(max_digits= 10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     currency = models.CharField(max_length=3)   
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
     purchase_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
