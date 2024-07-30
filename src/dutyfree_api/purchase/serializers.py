719fbc73d04dcfee6ab882e0a70860363518103c
Saifeddine <saifeddine.bejaoui@gmail.com>
2024-07-30 09:20:18 +0200

    Add Purchase Serializers, ViewSets and Routes

--- src/dutyfree_api/purchase/serializers.py
+++ src/dutyfree_api/purchase/serializers.py
@@ -1,2 +1,22 @@
 from rest_framework import serializers
 from .models import Customers, Purchases
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchases
+        exclude = ['customer']
+        
+
+class CustomerSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Customers
+        exclude = ['id']
+        
+    def create(self, validated_data):
+        purchase_data = validated_data.pop('purchases')
+        customer = Customers.objects.create(**validated_data)
+        for purchase in purchase_data:
+            purchase['customer'] = customer
+            Purchases.objects.create(**purchase)
+        return customer
--- src/dutyfree_api/purchase/views.py
+++ src/dutyfree_api/purchase/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Customers
+from .serializers import CustomerSerializer
 
-# Create your views here.
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customers.objects.all()
+    serializer_class = CustomerSerializer
