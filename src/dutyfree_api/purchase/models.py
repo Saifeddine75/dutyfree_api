c5c1d7e10bca1d456fd4afc5927dc2a23f69f054
Saifeddine <saifeddine.bejaoui@gmail.com>
2024-07-30 13:51:51 +0200

    Update purchase models fields: currency, puchased_at

--- src/dutyfree_api/purchase/models.py
+++ src/dutyfree_api/purchase/models.py
@@ -17,9 +17,9 @@ class Purchases(models.Model):
     customer =  models.ForeignKey('Customers', related_name='purchases', on_delete=models.CASCADE)
     product_id = models.CharField(max_length=20)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    currency = models.CharField(max_length=3)   
+    currency = models.CharField(max_length=20)   
     quantity = models.PositiveIntegerField()
-    purchase_at = models.DateTimeField(auto_now_add=True)
+    purchased_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.product_id
