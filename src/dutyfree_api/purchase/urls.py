45c6f4a091d0e2c88a8ca12bfea1090cced380f6
Saifeddine <saifeddine.bejaoui@gmail.com>
2024-08-10 14:16:10 +0200

    update .gitignore

--- src/dutyfree_api/purchase/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from purchase.views import CustomerCreateListAPIView
-
-urlpatterns = [
-    path('customers/', CustomerCreateListAPIView.as_view(), name='customer-create-list'),
-]
--- src/post_json_payload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://127.0.0.1:8000/v1/customers/ \
-    -H "Content-Type: application/json" \
-    -d @data/payload.json
